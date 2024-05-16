4daa541c0ca6dfe4fb55cb76b6519208662deb04
YomnaMagdy21 <93162207+YomnaMagdy21@users.noreply.github.com>
2024-05-17 02:53:46 +0300

    Home view

---
+++ SportsApp/HomeCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  HomeCollectionViewCell.swift
+//  SportsApp
+//
+//  Created by Yomna Magdy  on 5/16/24.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var img: UIImageView!
+}
