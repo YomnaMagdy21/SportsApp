20ccdf685cfbfcb9a83a9e831cbc885e90c65339
Omaar Mohamed <102541769+Omaar-Mohamed@users.noreply.github.com>
2024-05-18 20:06:47 +0300

    adding cell solving conflict 2

---
+++ SportsApp/LeaguesUpCommingEventsCollectionViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  LeaguesUpCommingEventsCollectionViewCell.swift
+//  SportsApp
+//
+//  Created by Omar  on 17/05/2024.
+//
+
+import UIKit
+
+class LeaguesUpCommingEventsCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var firstTeamImage: UIImageView!
+    
+    @IBOutlet weak var firstTeamName: UILabel!
+    @IBOutlet weak var secTeamImage: UIImageView!
+    @IBOutlet weak var secTeamName: UILabel!
+    @IBOutlet weak var matchDate: UILabel!
+    @IBOutlet weak var matchTime: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        //adding commient
+        
+    }
+
+}
