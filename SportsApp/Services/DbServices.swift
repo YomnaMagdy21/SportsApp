518e8f6e2ef4dc5ef310571c3bb339c06b6123ce
YomnaMagdy21 <93162207+YomnaMagdy21@users.noreply.github.com>
2024-05-25 02:54:59 +0300

    fetch data

--- SportsApp/Services/DbServices.swift
+++ SportsApp/Services/DbServices.swift
@@ -7,10 +7,43 @@
 
 import Foundation
 import CoreData
+import UIKit
 
 protocol DbServices {
-    
+    func fetchLeaguesData()
 }
 class DbServicesImpl : DbServices{
+    
+    let appDelegate : AppDelegate
+    let context : NSManagedObjectContext
+    var leagues : [NSManagedObject] = []
+    
+    init(){
+        appDelegate = UIApplication.shared.delegate as! AppDelegate
+        context = self.appDelegate.persistentContainer.viewContext
+        
+    }
+    
+    func fetchLeaguesData() {
+        
+        let fetchRequest = NSFetchRequest<NSManagedObject>(
+            entityName: "FavLeagues")
+        
+        do{
+            self.leagues = try context.fetch(fetchRequest)
+            var league = League()
+            for fav in leagues{
+               
+                fav.value(forKey: "league_name")
+                fav.value(forKey: "league_logo")
+                fav.value(forKey: "league_key")
+                leagues.append(fav)
+            }
+            
+        }catch{
+            print("Failed to fetch data: \(error.localizedDescription)")
+        }
+    }
+   
     
 }
