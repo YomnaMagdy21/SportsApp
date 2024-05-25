418da09f57c2442cd5dd3a0c5df607c2657a5522
YomnaMagdy21 <93162207+YomnaMagdy21@users.noreply.github.com>
2024-05-25 13:06:54 +0300

    check data

--- SportsApp/Services/DbServices.swift
+++ SportsApp/Services/DbServices.swift
@@ -11,17 +11,20 @@ import UIKit
 
 protocol DbServices {
     func fetchLeaguesData()
+    func checkLeaguesData() -> Bool 
+
 }
 class DbServicesImpl : DbServices{
     
     let appDelegate : AppDelegate
     let context : NSManagedObjectContext
     var leagues : [NSManagedObject] = []
+    let league : League
     
     init(){
         appDelegate = UIApplication.shared.delegate as! AppDelegate
         context = self.appDelegate.persistentContainer.viewContext
-        
+        league = League()
     }
     
     func fetchLeaguesData() {
@@ -44,6 +47,22 @@ class DbServicesImpl : DbServices{
             print("Failed to fetch data: \(error.localizedDescription)")
         }
     }
+    func checkLeaguesData() -> Bool {
+       
+        let fetchRequest = NSFetchRequest<NSManagedObject>(
+            entityName: "FavLeagues")
+        let predicate = NSPredicate(format: "league_name == %@", league.league_name ?? "no name")
+        fetchRequest.predicate = predicate
+        
+        do {
+            let fav = try context.fetch(fetchRequest)
+            return !fav.isEmpty
+        } catch {
+            print("failed to get data: \(error.localizedDescription)")
+            return false
+        }
+    }
+
    
     
 }
