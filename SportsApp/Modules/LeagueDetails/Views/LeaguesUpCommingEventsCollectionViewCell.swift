af44393bec42d66d27bac6cc14a2dfcb04e737b5
YomnaMagdy21 <93162207+YomnaMagdy21@users.noreply.github.com>
2024-05-29 03:34:25 +0300

    edit in image

--- SportsApp/Modules/LeagueDetails/Views/LeaguesUpCommingEventsCollectionViewCell.swift
+++ SportsApp/Modules/LeagueDetails/Views/LeaguesUpCommingEventsCollectionViewCell.swift
@@ -24,10 +24,20 @@ class LeaguesUpCommingEventsCollectionViewCell: UICollectionViewCell {
         // Initialization code
         //adding commient
         setupUI()
+//        
+//        imgView.frame = CGRect(x: 1, y: 40, width: frame.width, height: frame.height - 40)
+//        addSubview(imgView)
         
+    }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
         imgView.frame = CGRect(x: 1, y: 40, width: frame.width, height: frame.height - 40)
         addSubview(imgView)
-        
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     private func setupUI() {
          // Set border color and width
